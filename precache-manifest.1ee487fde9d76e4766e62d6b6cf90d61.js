eba6a6b51762ce24955a0852d5d651802ee81ba0
Minh Pham <omiph001@gmail.com>
2019-02-24 17:24:26 +0200

    Updates

---
+++ precache-manifest.1ee487fde9d76e4766e62d6b6cf90d61.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f740977fd095a96365c4",
+    "url": "/portfolio-react-app/static/css/main.a2abeedc.chunk.css"
+  },
+  {
+    "revision": "f740977fd095a96365c4",
+    "url": "/portfolio-react-app/static/js/main.f740977f.chunk.js"
+  },
+  {
+    "revision": "bb060588f96b81a87069",
+    "url": "/portfolio-react-app/static/css/1.581ba1ee.chunk.css"
+  },
+  {
+    "revision": "bb060588f96b81a87069",
+    "url": "/portfolio-react-app/static/js/1.bb060588.chunk.js"
+  },
+  {
+    "revision": "82922a53ddcd8a6bfd0a",
+    "url": "/portfolio-react-app/static/js/runtime~main.82922a53.js"
+  },
+  {
+    "revision": "1c034a3c79b7bb221891bf22588b450d",
+    "url": "/portfolio-react-app/static/media/AVENGEANCE HEROIC AVENGER.1c034a3c.ttf"
+  },
+  {
+    "revision": "b00883872385642d510b3aab030f19d2",
+    "url": "/portfolio-react-app/static/media/About.b0088387.jpg"
+  },
+  {
+    "revision": "4ecb0c6ee6e829cabb00e38f16564baa",
+    "url": "/portfolio-react-app/static/media/Home.4ecb0c6e.jpeg"
+  },
+  {
+    "revision": "e0596823f3662a71316f3ff0a053d2b8",
+    "url": "/portfolio-react-app/static/media/space-mono-latin-400.e0596823.woff2"
+  },
+  {
+    "revision": "101b9f2b931e213c17e90dc393e41544",
+    "url": "/portfolio-react-app/static/media/space-mono-latin-700.101b9f2b.woff2"
+  },
+  {
+    "revision": "478be5364d73478dfe8eab68ab8d26ad",
+    "url": "/portfolio-react-app/static/media/space-mono-latin-400.478be536.woff"
+  },
+  {
+    "revision": "6f0434667fae13084dd77a10c9099ff8",
+    "url": "/portfolio-react-app/static/media/space-mono-latin-400italic.6f043466.woff2"
+  },
+  {
+    "revision": "904f140a0d640bdbf361c42135b98a80",
+    "url": "/portfolio-react-app/static/media/space-mono-latin-400italic.904f140a.woff"
+  },
+  {
+    "revision": "5ec6d1770ee32ed03cbd793416397904",
+    "url": "/portfolio-react-app/static/media/space-mono-latin-700italic.5ec6d177.woff2"
+  },
+  {
+    "revision": "17706b332de51a4c0dae838259750549",
+    "url": "/portfolio-react-app/static/media/space-mono-latin-700.17706b33.woff"
+  },
+  {
+    "revision": "dfcae625c4a436325b7a7b90e4339080",
+    "url": "/portfolio-react-app/static/media/space-mono-latin-700italic.dfcae625.woff"
+  },
+  {
+    "revision": "c787d9b8bebe264fde059af8c1301ae6",
+    "url": "/portfolio-react-app/index.html"
+  }
+];
